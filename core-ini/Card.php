82b7f20258f905ecfaf5224530a4749bca4199b8
Ghost <30910719+Ghost-J@users.noreply.github.com>
2017-08-12 17:01:43 +0200

    Added the getters

--- core-ini/Card.php
+++ core-ini/Card.php
@@ -6,10 +6,10 @@
  * Time: 08:57
  */
 
-class Card
+final class Card
 {
    private $color;
-   public $value;
+   private $value;
    private $face;
    private $suit;
 
@@ -20,7 +20,7 @@ class Card
     *
     * @param (int)    value between 1-13
     * @param (String) black or red
-    * @param (String) spades, cross, hearts, diamond
+    * @param (String) spades, clubs, hearts, diamond
     * @param (bool)   1 = face up , 0 = face down
     */
    public function __construct($value, $color, $suit, $face) {
@@ -31,13 +31,57 @@ class Card
    }
 
    /*
+    * getValue
     *
+    * Gets the value of the card.
+    *
+    * @param (void)
+    *
+    * @return (int) A value between 1 and 13
     */
-   //public function setValue($value) {
+   public function getValue() {
+       return $this->value;
+   }
 
-   //}
+    /*
+     * getColor
+     *
+     * Gets the color of the card.
+     *
+     * @param (void)
+     *
+     * @return (String) "red" || "black"
+     */
+   public function  getColor() {
+       return $this->color;
+   }
 
+    /*
+     * getSuit
+     *
+     * Gets the suit of the card.
+     *
+     * @param (void)
+     *
+     * @return (String) spades, clubs, hearts, diamond
+     */
+   public function getSuit() {
+       return $this->suit;
+   }
+
+    /*
+      * getFace
+      *
+      * Gets the face of the card.
+      *
+      * @param (void)
+      *
+      * @return (bool) 1 for open, 0 for set
+      */
+   public function  getFace() {
+       return $this->face;
+   }
 }
 
 $card = new Card(10, 'red', "hearts", 1);
-echo $card->value;
+echo $card->getValue();
